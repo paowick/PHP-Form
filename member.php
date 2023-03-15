d0354bd385fd14bbb3f571c94e6687ae33d86f1b
korrakrit gumnarai <korn2k9@gmail.com>
2023-03-15 15:53:42 +0700

    final

--- member.php
+++ member.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <?php
-        echo 'First Name : '.$_POST["fname"].'<br>';
-        echo 'Last name : '.$_POST["lname"].'<br>';
-        echo 'Phone Number : '.$_POST["number"].'<br>';
-        echo 'Email : '.$_POST["email"].'<br>';
-        echo 'Password : '.$_POST["password"].'<br>';
-    
-    ?>
+    <main>
+        <div class="form">
+            <?php
+            echo 'First Name : ' . $_POST["fname"] . '<br>';
+            echo 'Last name : ' . $_POST["lname"] . '<br>';
+            echo 'Phone Number : ' . $_POST["number"] . '<br>';
+            echo 'Email : ' . $_POST["email"] . '<br>';
+            echo 'Password : ' . $_POST["password"] . '<br>';
+            ?>
+
+        </div>
+
+    </main>
 </body>
+
 </html>
